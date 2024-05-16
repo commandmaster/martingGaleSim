86d8aff867e31ee76ae99ac1aa08d0e1eb5ad3d0
commandmaster <153877155+commandmaster@users.noreply.github.com>
2024-05-16 07:39:16 -0600

    it will not work

--- simTester.js
+++ simTester.js
@@ -13,23 +13,21 @@ const simTester = (maxSimulations, startingMoney, startingBet, winChance, winMul
 }
 
 async function testMultiplyers(){
-    const startingMoney = 1000;
-    const startingBet = 1;
-    const maxIterations = 1000;
+    const startingMoney = 10;
+    const startingBet = 0.01;
+    const maxIterations = 10000;
     const maxTime = 10;
-    const targetMoney = 2000;
-    const maxSimulations = 1000;
+    const targetMoney = 50;
+    const maxSimulations = 100000;
     const winMults = [9.9, 4.95, 3.3, 2.475, 1.98, 1.65, 1.4143, 1.2375, 1.1];
     const winChances = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
     const results = [];
 
     for (let i = 0; i < winMults.length; i++){
-        for (let j = 0; j < winChances.length; j++){
-            const winChance = winChances[j];
-            const winMult = winMults[i];
-            const result = await simTester(maxSimulations, startingMoney, startingBet, winChance, winMult, maxIterations, maxTime, targetMoney);
-            results.push(result);
-        }
+        const winChance = winChances[i];
+        const winMult = winMults[i];
+        const result = await simTester(maxSimulations, startingMoney, startingBet, winChance, winMult, maxIterations, maxTime, targetMoney);
+        results.push(JSON.parse(result));
     }
 
     fs.writeFile('results.json', JSON.stringify(results, null, 4), (err) => {
@@ -39,3 +37,5 @@ async function testMultiplyers(){
     });
    
 }
+
+testMultiplyers();
